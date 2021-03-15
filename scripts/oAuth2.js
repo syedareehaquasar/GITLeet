91a3dad89316c0d49fd3694b0f054691af4cbba3
syedareehaquasar <reeha.saba@gmail.com>
2021-03-16 01:20:07 +0530

    adding all work done till now to get authorization token

---
+++ scripts/oAuth2.js
@@ -0,0 +1,38 @@
+const oAuth2 = {
+    /**
+     * Initialize
+     */
+    init() {
+        this.KEY = 'gitleet_token';
+        this.ACCESS_TOKEN_URL =
+            'https://github.com/login/oauth/access_token';
+        this.AUTHORIZATION_URL =
+            'https://github.com/login/oauth/authorize';
+        this.REDIRECT_URL = 'https://github.com/'; // for example, https://github.com
+        this.SCOPES = ['repo'];
+    },
+
+    /**
+     * Begin
+     */
+    begin() {
+        this.init(); // secure token params.
+
+        let url = `${this.AUTHORIZATION_URL}?client_id=${this.CLIENT_ID}&redirect_uri${this.REDIRECT_URL}&scope=`;
+
+        for (let i = 0; i < this.SCOPES.length; i += 1) {
+            url += this.SCOPES[i];
+        }
+
+        chrome.storage.local.set({ pipe_gitleet: true }, () => {
+            // opening pipe temporarily
+
+            chrome.tabs.create({ url, selected: true }, function() {
+                window.close();
+                chrome.tabs.getCurrent(function(tab) {
+                    chrome.tabs.remove(tab.id, function() {});
+                });
+            });
+        });
+    },
+};
